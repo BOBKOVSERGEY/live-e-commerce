3741fcc2c1fab3336039c6066544088a76ca78d4
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2019-06-10 22:22:18 +0300

    detail

--- app/views/product/details.php
+++ app/views/product/details.php
@@ -1,6 +1,29 @@
 <?php $this->start('head'); ?>
-<script>console.log('1')</script>
+
 <?php $this->end(); ?>
 <?php $this->start('body'); ?>
-  <h1>Some product</h1>
+<h1>Some product</h1>
+<div class="row">
+  <div class="col-md-6">
+    <img src="https://cdn10.bigcommerce.com/s-cbajx/products/7157/images/8911/GUEST_9d25776b-4e83-4a12-a25f-2c78489b1222__64102.1546117487.1280.1280.jpg" alt="">
+  </div>
+  <div class="col-md-6">
+    <p>Price</p>
+    <p>OldPrice</p>
+    <p>Shipping</p>
+    <p>Vendor</p>
+    <div>
+      <button class="btn btn-info"><i class="fas fa-shopping-cart"></i> Add to cart</button>
+    </div>
+  </div>
+</div>
+<div class="row mt-5">
+  <div class="col-md-6">
+    <h3>Product description</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus eaque esse facere maiores nemo sapiente suscipit tempora totam? Ab accusantium aliquam amet eaque, hic iure molestiae porro quae voluptate!</p>
+  </div>
+  <div class="col-md-6">
+    <h3>Customer Reviews</h3>
+  </div>
+</div>
 <?php $this->end(); ?>
